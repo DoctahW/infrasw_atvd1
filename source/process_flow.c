6f8cc5823b7369faef461f494f064d17b6f0c3d0
Euclides <joaomartires2@gmail.com>
2026-08-18 23:47:45 -0300

    feat: laço de leitura basico

--- source/process_flow.c
+++ source/process_flow.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
-bool isappopen = true;
+bool running = true;
 
-int main (int argc, char *argv[]){
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
-            printf("Lendo o arquivo: %s",argv[i]);
+void leitura(FILE *arquivo, int modo){
+    char buffer[300];
+    
+    while (running) {
+        if (modo == 1){
+            printf("processflow>");
+        }
+        char *result = fgets(buffer, 300, arquivo);
+        if (result == NULL) {
+            break;
+        }
+
+        if (modo == 0){
+            printf("%s", buffer);
+        }
+        
+        if (strcmp(buffer, "exit\n") == 0) {
+            running = false;
         }
     }
-    else {
-        printf("Iniciando modo interativo...");
+}
+
+int main (int argc, char *argv[]){
+    if (argc == 1) {
+        leitura(stdin, 1);
+    }
+    if (argc == 2) {
+        FILE *arquivo = fopen(argv[1], "r");
+        if (arquivo == NULL) {
+            printf("Erro: arquivo não encontrado.\n");
+            return 1;
+        }
+        leitura(arquivo, 0);
+        fclose(arquivo);
+    }
+    if (argc > 2) {
+        printf("Erro: número de argumentos inválido.\n");
+        return 1;
     }
 }
